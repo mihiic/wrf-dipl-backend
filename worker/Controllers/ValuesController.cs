781a62d1c9ea1cc2fe5b3164c6165a7d2aac01a9
Mihael Medan <mmedan@xtreme.bet>
2019-01-20 14:06:30 +0100

    init commit (for testing deployment)

---
+++ worker/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace worker.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() {
+            return new string[] {"value1", "value2"};
+        }
+    }
+}
